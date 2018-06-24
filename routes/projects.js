abd8dde049bec6b4c69409bed4db34321f87a409
Little Einstien <arnavsimer@ymail.com>
2018-06-24 13:37:55 +0530

    Update projects.js

--- routes/projects.js
+++ routes/projects.js
@@ -208,7 +208,7 @@ function mkDirByPathSync(targetDir, { isRelativeToScript = false } = {}) {
     const sep = path.sep;
     const initDir = path.isAbsolute(targetDir) ? sep : '';
 //     const baseDir = 'F:/';
-    const baseDir = '/home/einstien0001/Arnav/Picasi';
+    const baseDir = '/home/einstien0001/Arnav/NLU_backend';
     console.log(targetDir.split(sep));
     targetDir.split(sep).reduce((parentDir, childDir) => {
         const curDir = path.resolve(baseDir, parentDir, childDir);
